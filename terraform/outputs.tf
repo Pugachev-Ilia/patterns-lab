256258167fcb93a209db40388c929e4c6bac3f91
ilia pugachev <puagchev.ilay@gmail.com>
2026-01-26 01:33:32 +0400

    Expose ECS identifiers for CI/CD vars

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -7,3 +7,16 @@ output "ecr_repository_url" {
 output "alb_dns_name" {
   value = module.ecs_service.alb_dns_name
 }
+
+# ECS identifiers (for CI/CD)
+output "ecs_cluster_name" {
+  value = module.ecs_cluster.cluster_name
+}
+
+output "ecs_service_name" {
+  value = module.ecs_service.service_name
+}
+
+output "ecs_container_name" {
+  value = "app"
+}
