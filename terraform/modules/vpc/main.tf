30a4e3f09ad53962d142cca59ff6cc94a0e784cf
ilia pugachev <puagchev.ilay@gmail.com>
2026-01-26 01:33:20 +0400

    Fix public route to IGW for NAT egress

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -37,6 +37,12 @@ resource "aws_route_table" "public" {
   tags = merge(local.tags, { Name = "${local.base_name}-rt-public" })
 }
 
+resource "aws_route" "public_0_0_0_0" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.this.id
+}
+
 resource "aws_route_table_association" "public" {
   for_each       = aws_subnet.public
   subnet_id      = each.value.id
